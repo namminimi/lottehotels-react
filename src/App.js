eb5a073fac2315b6f1763ad0407cfd442c52a4f2
namminimi <tbvhdaos@naver.com>
2023-02-08 14:05:33 +0900

    lottehotels-react

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import IdFind from './pages/member/IdFind';
 import JoinPage from './pages/member/JoinPage';
 import Login from './pages/member/Login';
 import PassFind from './pages/member/PassFind';
+import WriteEvent from './pages/WriteEvent';
 
 
 
@@ -28,6 +29,7 @@ function App() {
       <Route path='/findid' element={<IdFind/>}/>
       <Route path='/findpass' element={<PassFind/>}/>
       <Route path='/updatepass' element={<EditPassword/>}/>
+      <Route path='/writeevent' element={<WriteEvent/>}/>
      </Routes>
      <Footer/>
     </div>
